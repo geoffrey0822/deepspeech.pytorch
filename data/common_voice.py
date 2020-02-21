7641c6a05b4dc2744b6ec8c545f1d04779cd8875
geoffrey0822 <geoffrey_poon@ieee.org>
2020-02-21 13:31:35 +0800

    Bug fixed

--- data/common_voice.py
+++ data/common_voice.py
@@ -19,9 +19,12 @@ parser.add_argument('--files-to-process', default="cv-valid-dev.csv,cv-valid-tes
                     type=str, help='list of *.csv file names to process')
 parser.add_argument('--corpus-subdir', default="cv_corpus_v1",
                     type=str, help='Sub directory of corpus')
+parser.add_argument('--media-subdir', default="",
+                    type=str, help='Sub directory of media files')
 args = parser.parse_args()
 COMMON_VOICE_URL = "https://common-voice-data-download.s3.amazonaws.com/cv_corpus_v1.tar.gz"
 
+media_path = None
 
 def convert_to_wav(csv_file, target_dir):
     """ Read *.csv file description, convert mp3 to wav, process text.
@@ -38,13 +41,16 @@ def convert_to_wav(csv_file, target_dir):
     path_to_data = os.path.dirname(csv_file)
 
     def process(x):
+        global media_path
         file_path, text = x
         file_name = os.path.splitext(os.path.basename(file_path))[0]
+        act_path_to_data = os.path.join(path_to_data, media_path)
+
         text = text.strip().upper()
         with open(os.path.join(txt_dir, file_name + '.txt'), 'w') as f:
             f.write(text)
         cmd = "sox {} -r {} -b 16 -c 1 {}".format(
-            os.path.join(path_to_data, file_path),
+            os.path.join(act_path_to_data, file_path),
             args.sample_rate,
             os.path.join(wav_dir, file_name + '.wav'))
         subprocess.call([cmd], shell=True)
@@ -68,6 +74,10 @@ def main():
     target_dir = args.target_dir
     os.makedirs(target_dir, exist_ok=True)
 
+    media_path = target_dir
+    if args.media_subdir is not None and args.media_subdir != '':
+        media_path = os.path.join(target_dir, args.media_subdir)
+
     target_unpacked_dir = os.path.join(target_dir, "CV_unpacked")
     os.makedirs(target_unpacked_dir, exist_ok=True)
 
@@ -88,6 +98,7 @@ def main():
         corpus_path = target_unpacked_dir
         if args.corpus_subdir is not None and args.corpus_subdir !='':
             corpus_path = os.path.join(target_unpacked_dir, args.corpus_subdir)
+
         convert_to_wav(os.path.join(corpus_path, csv_file),
                        os.path.join(target_dir, os.path.splitext(csv_file)[0]))
 
