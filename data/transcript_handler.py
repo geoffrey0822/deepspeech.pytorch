23185bba81b7224434cf5212472f0389ad0006ed
geoffrey0822 <geoffrey_poon@ieee.org>
2020-02-21 23:34:49 +0800

    customize for chinese content

--- data/transcript_handler.py
+++ data/transcript_handler.py
@@ -1,6 +1,7 @@
-import os, sys, re, argparse, csv, ntpath
+import os, sys, re, argparse, csv, ntpath, shutil
 
 TMP_DIR = 'tmp'
+
 
 def seg_char(sent):
     # handle english first
@@ -29,6 +30,7 @@ def add_2_dict(target_file, words, end_str=','):
             fs.write('\"%s\n'%end_str)
         else:
             fs.write('\"\n')
+    fs.close()
 
 
 def close_dict(target_file):
@@ -52,18 +54,26 @@ def buffer_dict_continue(target_file):
                     outputf.write('\n')
                     if not line.endswith('[') and not line.endswith(','):
                         break
+        shutil.copyfile(tmp_path, target_file)
         return True
+
 
 def path_leaf(path):
     head, tail = ntpath.split(path)
     return tail or ntpath.basename(head)
 
 
-def process_files(rec_file, dst, dict_file, simplified=False):
+def process_files(rec_file, dst, new_rec_file, dict_file, simplified=False):
+    buffer_dict_continue(dict_file)
     if not os.path.isdir(dst):
         os.mkdir(dst)
+    rec_f = None
+    if os.path.isfile(new_rec_file):
+        rec_f = open(new_rec_file, 'a+')
+    else:
+        rec_f = open(new_rec_file, 'w')
     with open(rec_file, 'r') as f:
-        reader = csv.reader(rec_file, delimiter=',')
+        reader = csv.reader(f, delimiter=',')
         for row in reader:
             audio_file = row[0]
             txt_file = row[1]
@@ -74,9 +84,9 @@ def process_files(rec_file, dst, dict_file, simplified=False):
                         line = ln.rstrip('\n')
                         cchar = seg_char(line)
                         outf.write(line)
-
-
-
+                        add_2_dict(dict_file, cchar)
+            rec_f.write('%s,%s\n'%(audio_file, new_file_path))
+    rec_f.close()
 
 
 def main():
@@ -85,9 +95,10 @@ def main():
     parser.add_argument('--data-file', type=str, help='Data record file')
     parser.add_argument('--dict', type=str, help='Dictionary output')
     parser.add_argument('--simplified', type=int, default=0, help='Is it simplified chinese?')
-    parser.add_argument('--dict-append', type=int, default=0, help='0-write mode, 1-append mode')
     parser.add_argument('--output-file', type=str, help='New data record file')
     args = parser.parse_args()
+
+    process_files(args.data_file, args.dst, args.output_file, args.dict, args.simplified == 1)
 
 
 if __name__ == "__main__":
