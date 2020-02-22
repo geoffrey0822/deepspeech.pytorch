130430bd297291453d6bfde5d69cf690158c8678
geoffrey0822 <geoffrey_poon@ieee.org>
2020-02-22 21:06:03 +0800

    customize for chinese content

--- data/transcript_handler.py
+++ data/transcript_handler.py
@@ -87,7 +87,7 @@ def reformat_dict(dict_file):
         last_char = unique_char[-1]
         for char in unique_char:
             outf.write('\"')
-            outf.write(char.rstrip(' '))
+            outf.write(char.replace(' ',''))
             outf.write('\"')
             if char != last_char:
                 outf.write(',\n')
