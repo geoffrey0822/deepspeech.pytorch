48e42dd6931a1b0edd865bafd2d12d104d0687fc
geoffrey0822 <geoffrey_poon@ieee.org>
2020-02-23 19:21:15 +0800

    customize for chinese content

--- data/transcript_handler.py
+++ data/transcript_handler.py
@@ -118,7 +118,7 @@ def process_files(rec_file, dst, new_rec_file, dict_file, simplified=False,
                         line = ln.rstrip('\n')
                         if cc is not None:
                             line = cc.convert(line)
-                        line = line.replace('(','').replace(')','').replace('_','').replace('~','')
+                        line = line.replace(' ','').replace('(','').replace(')','').replace('_','').replace('~','')
                         cchar = utils.seg_char(line)
                         outf.write(line)
                         add_2_dict(dict_file, cchar)
