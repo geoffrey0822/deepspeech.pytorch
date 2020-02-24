da696b59bc0d63941f2145502afb5f46d0a4540d
geoffrey0822 <geoffrey_poon@ieee.org>
2020-02-24 21:49:14 +0800

    customize for chinese content

--- test.py
+++ test.py
@@ -24,7 +24,7 @@ def evaluate(test_loader, device, model, decoder, target_decoder, save_output=Fa
     model.eval()
     total_cer, total_wer, num_tokens, num_chars = 0, 0, 0, 0
     output_data = []
-    for i, (data) in tqdm(enumerate(test_loader), total=len(test_loader), miniters =1):
+    for i, (data) in tqdm(enumerate(test_loader), total=len(test_loader), miniters =len(test_loader)/100):
         inputs, targets, input_percentages, target_sizes = data
         input_sizes = input_percentages.mul_(int(inputs.size(3))).int()
         inputs = inputs.to(device)
