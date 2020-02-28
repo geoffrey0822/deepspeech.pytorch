ef6c99d8bb4451f73e59e8e4dd35c772bedeac53
geoffrey0822 <geoffrey_poon@ieee.org>
2020-02-28 21:33:34 +0800

    customize for chinese content

--- test.py
+++ test.py
@@ -22,6 +22,7 @@ parser = add_decoder_args(parser)
 
 def evaluate(test_loader, device, model, decoder, target_decoder, save_output=False, verbose=False, half=False):
     #model.eval()
+    print('Evaluating')
     total_cer, total_wer, num_tokens, num_chars = 0, 0, 0, 0
     output_data = []
     #for i, (data) in tqdm(enumerate(test_loader), total=len(test_loader), miniters =len(test_loader)/100):
