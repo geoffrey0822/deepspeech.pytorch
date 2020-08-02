cd53369ea47ad757ede29d95d02898f39de1fa56
geoffrey0822 <geoffrey_poon@ieee.org>
2020-08-02 23:08:01 +0800

    bug fixed

--- data/data_loader.py
+++ data/data_loader.py
@@ -174,6 +174,7 @@ class SpectrogramDataset(Dataset, SpectrogramParser):
             transcript = transcript_file.read().replace('\n', '')
             #transcript = seg_char(transcript)
             transcript = transcript.split(' ')
+            print(transcript)
         transcript = list(filter(None, [self.labels_map.get(x) for x in list(transcript)]))
         return transcript
 
